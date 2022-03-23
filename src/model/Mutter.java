1fa88c2829be42d7f4f6ff5d88d2a99ecb6e08ce
jspmiyawaki <jspmiyawaki@gmail.com>
2022-03-23 16:15:45 +0900

    追記
    以降、docotsubuDAOへ

--- src/model/Mutter.java
+++ src/model/Mutter.java
@@ -3,12 +3,16 @@ package model;
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
+@ToString
 public class Mutter implements Serializable{
 	private String userName;
 	private String text;
